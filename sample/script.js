89781a85a2b9e784c1748ad901426b7f34612da0
hassan asadollahi <hasan.asadolahi2@gmail.com>
2023-10-29 14:04:19 +0330

    react-ckeditor-4

--- sample/script.js
+++ sample/script.js
@@ -1,23 +1,62 @@
-createDialog().then( config => {
-	return ClassicEditor
-		.create( document.querySelector( '.editor' ), {
-			licenseKey: config.licenseKey,
-			ckbox: {
-				tokenUrl: config.ckboxTokenUrl
-			}
-		} )
+const watchdog = new CKSource.EditorWatchdog();
+
+window.watchdog = watchdog;
+
+watchdog.setCreator( ( element, config ) => {
+	return CKSource.Editor
+		.create( element, config )
 		.then( editor => {
-			window.editor = editor;
-		} )
-		.catch( handleSampleError );
+			return editor;
+		} );
 } );
+
+watchdog.setDestructor( editor => {
+	return editor.destroy();
+} );
+
+watchdog.on( 'error', handleSampleError );
+
+createDialog().then( config => watchdog.create(
+	document.querySelector( '.editor' ), {
+		licenseKey: config.licenseKey,
+		ckbox: {
+			tokenUrl: config.ckboxTokenUrl
+		},
+		sidebar: {
+			container: document.querySelector( '.sidebar' )
+		},
+		extraPlugins: [
+			// Learn more about users at https://ckeditor.com/docs/ckeditor5/latest/features/collaboration/users.html.
+			class UsersInit {
+				static get requires() {
+					return [ 'Users' ];
+				}
+
+				constructor( editor ) {
+					this.editor = editor;
+				}
+
+				init() {
+					const users = this.editor.plugins.get( 'Users' );
+
+					users.addUser( {
+						id: 'u1'
+					} );
+
+					users.defineMe( 'u1' );
+				}
+			}
+		]
+	}
+) )
+	.catch( handleSampleError );
 
 function handleSampleError( error ) {
 	const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
 
 	const message = [
 		'Oops, something went wrong!',
-		`Please, report the following error on ${ issueUrl } with the build id "b6ewy97lcyi-nohdljl880ze" and the error stack trace:`
+		`Please, report the following error on ${ issueUrl } with the build id "uz7jxto08aby-nohdljl880ze" and the error stack trace:`
 	].join( '\n' );
 
 	console.error( message );
--- src/ckeditor.ts
+++ src/ckeditor.ts
@@ -5,20 +5,29 @@
 
 import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
 
-import { AIAssistant } from '@ckeditor/ckeditor5-ai';
 import { Alignment } from '@ckeditor/ckeditor5-alignment';
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Code, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { Autosave } from '@ckeditor/ckeditor5-autosave';
+import {
+	Bold,
+	Italic,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Underline
+} from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import { CKBox } from '@ckeditor/ckeditor5-ckbox';
 import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+import { Comments } from '@ckeditor/ckeditor5-comments';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { ExportPdf } from '@ckeditor/ckeditor5-export-pdf';
 import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
-import { FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
-import { Heading } from '@ckeditor/ckeditor5-heading';
+import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading, Title } from '@ckeditor/ckeditor5-heading';
+import { Highlight } from '@ckeditor/ckeditor5-highlight';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
 import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
-import { DataSchema } from '@ckeditor/ckeditor5-html-support';
+import { DataSchema, GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
 import {
 	AutoImage,
 	Image,
@@ -28,18 +37,41 @@ import {
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
-	PictureEditing,
-	ImageEditing
+	PictureEditing
 } from '@ckeditor/ckeditor5-image';
 import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link, LinkImage } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
+import { TextPartLanguage } from '@ckeditor/ckeditor5-language';
+import { AutoLink, Link, LinkImage } from '@ckeditor/ckeditor5-link';
+import { List, ListProperties, TodoList } from '@ckeditor/ckeditor5-list';
+import { Markdown } from '@ckeditor/ckeditor5-markdown-gfm';
 import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
-import { Pagination } from '@ckeditor/ckeditor5-pagination';
+import { PageBreak } from '@ckeditor/ckeditor5-page-break';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { StandardEditingMode } from '@ckeditor/ckeditor5-restricted-editing';
+import { SelectAll } from '@ckeditor/ckeditor5-select-all';
+import { ShowBlocks } from '@ckeditor/ckeditor5-show-blocks';
+import {
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText
+} from '@ckeditor/ckeditor5-special-characters';
+import { Style } from '@ckeditor/ckeditor5-style';
+import {
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableProperties,
+	TableToolbar
+} from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import { EditorWatchdog } from '@ckeditor/ckeditor5-watchdog';
+import { WordCount } from '@ckeditor/ckeditor5-word-count';
 import MathType from '@wiris/mathtype-ckeditor5';
 
 // You can read more about extending the build with additional plugins in the "Installing plugins" guide.
@@ -47,23 +79,27 @@ import MathType from '@wiris/mathtype-ckeditor5';
 
 class Editor extends ClassicEditor {
 	public static override builtinPlugins = [
-		AIAssistant,
 		Alignment,
 		AutoImage,
+		AutoLink,
 		Autoformat,
+		Autosave,
 		BlockQuote,
 		Bold,
 		CKBox,
 		CloudServices,
-		Code,
+		Comments,
 		DataSchema,
 		Essentials,
-		ExportPdf,
 		FindAndReplace,
+		FontBackgroundColor,
 		FontColor,
 		FontFamily,
 		FontSize,
+		GeneralHtmlSupport,
 		Heading,
+		Highlight,
+		HorizontalLine,
 		HtmlEmbed,
 		Image,
 		ImageCaption,
@@ -72,21 +108,45 @@ class Editor extends ClassicEditor {
 		ImageStyle,
 		ImageToolbar,
 		ImageUpload,
-		ImageEditing,
 		Indent,
 		Italic,
 		Link,
 		LinkImage,
 		List,
+		ListProperties,
+		Markdown,
 		MathType,
 		MediaEmbed,
-		Pagination,
+		PageBreak,
 		Paragraph,
 		PasteFromOffice,
 		PictureEditing,
+		SelectAll,
+		ShowBlocks,
+		SpecialCharacters,
+		SpecialCharactersArrows,
+		SpecialCharactersCurrency,
+		SpecialCharactersEssentials,
+		SpecialCharactersLatin,
+		SpecialCharactersMathematical,
+		SpecialCharactersText,
+		StandardEditingMode,
+		Strikethrough,
+		Style,
+		Subscript,
+		Superscript,
 		Table,
+		TableCaption,
+		TableCellProperties,
+		TableColumnResize,
+		TableProperties,
 		TableToolbar,
-		TextTransformation
+		TextPartLanguage,
+		TextTransformation,
+		Title,
+		TodoList,
+		Underline,
+		WordCount
 	];
 
 	public static override defaultConfig = {
@@ -114,6 +174,7 @@ class Editor extends ClassicEditor {
 		language: 'fa',
 		image: {
 			toolbar: [
+				'comment',
 				'imageTextAlternative',
 				'toggleImageCaption',
 				'imageStyle:inline',
@@ -126,20 +187,25 @@ class Editor extends ClassicEditor {
 			contentToolbar: [
 				'tableColumn',
 				'tableRow',
-				'mergeTableCells'
+				'mergeTableCells',
+				'tableCellProperties',
+				'tableProperties'
+			],
+			tableToolbar: [
+				'comment'
 			]
 		},
-		pagination: {
-			pageWidth: '21cm',
-			pageHeight: '29.7cm',
-			pageMargins: {
-				top: '20mm',
-				bottom: '20mm',
-				left: '12mm',
-				right: '12mm'
+		comments: {
+			editorConfig: {
+				extraPlugins: [
+					Autoformat,
+					Bold,
+					Italic,
+					List
+				]
 			}
 		}
 	};
 }
 
-export default Editor;
+export default { Editor, EditorWatchdog };
